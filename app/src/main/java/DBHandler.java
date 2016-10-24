a49fce045eba20df4a76d1ae047dd3628ec4e131
pqueralt <pascalequeralt@gmail.com>
2016-10-24 14:43:52 -0500

    Database handler updates

--- app/src/main/java/DBHandler.java
+++ app/src/main/java/DBHandler.java
@@ -10,36 +10,63 @@ public class DBHandler extends SQLiteOpenHelper {
 
     // Database Version
     private static final int DATABASE_VERSION = 1;
+
     // Database Name
-    private static final String DATABASE_NAME = "recipeInfo";
+    private static final String DATABASE_NAME = "grocerEaseData";
+
     // Contacts table name
     private static final String TABLE_RECIPES = "recipes";
-    // Shops Table Columns names
-    private static final String KEY_ID = "recipe_id";
-    private static final String KEY_TITLE = "recipe_title";
-    private static final String KEY_DESCRIPTION = "recipe_description";
-    public DBHandler(Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
-    }
+    //private static final String TABLE_INGREDIENTS = "ingredients";
+    //private static final String TABLE_RECIPES_INGREDIENTS = "recipes_ingredients";
+
+    //Common Column names
+    private static final String KEY_ID = "id";
+
+    //RECIPES TABLE column names
+    private static final String KEY_TITLE = "title";
+    private static final String KEY_CUISINES = "cuisines";
+    private static final String KEY_MINUTES = "minutes";
+    private static final String KEY_IMAGE = "image";
+    private static final String KEY_IMAGE_TYPE = "image_type";
+    private static final String KEY_INSTRUCTIONS = "instructions";
+    private static final String KEY_SERVINGS = "servings";
+    private static final String KEY_POPULAR = "popular";
+    private static final String KEY_HEALTHY = "healthy";
+    private static final String KEY_VEGETARIAN = "vegetarian";
+    private static final String KEY_VEGAN = "vegan";
+    private static final String KEY_GLUTEN_FREE = "gluten_free";
+    private static final String KEY_DAIRY_FREE = "dairy_free";
+    private static final String KEY_CHEAP = "cheap";
+
+    //Table Create Statements
+    //Recipes Table create statement
+    private static final String CREATE_TABLE_RECIPES = "CREATE TABLE "
+            + TABLE_RECIPES + "(" + KEY_ID + " INTEGER PRIMARY KEY," + KEY_TITLE
+            + " TEXT," + KEY_CUISINES + " TEXT," + KEY_MINUTES + " INTEGER," +
+            KEY_IMAGE + " TEXT," + KEY_IMAGE_TYPE + " TEXT," + KEY_INSTRUCTIONS +
+            " TEXT," + KEY_SERVINGS + " INTEGER," + KEY_POPULAR + " BOOLEAN," +
+            KEY_HEALTHY + " BOOLEAN," + KEY_VEGETARIAN + " BOOLEAN," + KEY_VEGAN + "BOOLEAN,"
+            + KEY_GLUTEN_FREE + " BOOLEAN," + KEY_DAIRY_FREE + " BOOLEAN," + KEY_CHEAP + " BOOLEAN,"
+            + ")";
+
+
     @Override
     public void onCreate(SQLiteDatabase db) {
-        String CREATE_CONTACTS_TABLE = "CREATE TABLE " + TABLE_RECIPES + "("
-        + KEY_ID + " INTEGER PRIMARY KEY," + KEY_TITLE + " TEXT,"
-        + KEY_DESCRIPTION + " TEXT" + ")";
-        db.execSQL(CREATE_CONTACTS_TABLE);
+
+        // creating required tables
+        db.execSQL(CREATE_TABLE_RECIPES);
+
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    // Drop older table if existed
-            db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECIPES);
-    // Creating tables again
-            onCreate(db);
+
+        // on upgrade drop older tables
+        db.execSQL("DROP TABLE IF EXISTS " + TABLE_RECIPES);
+
     }
 
-
-    //Adding a new recipes
-    public void addRecipe(){
-
+    public DBHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
 
 }
