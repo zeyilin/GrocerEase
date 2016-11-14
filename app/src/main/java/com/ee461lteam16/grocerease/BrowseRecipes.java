0ee69a29d70e287d9ae2ac9461888c27bd38c124
pqueralt <pascalequeralt@gmail.com>
2016-11-13 20:35:01 -0600

    Display first ten recipes from Json on browse recipe page

--- app/src/main/java/com/ee461lteam16/grocerease/BrowseRecipes.java
+++ app/src/main/java/com/ee461lteam16/grocerease/BrowseRecipes.java
@@ -2,9 +2,16 @@ package com.ee461lteam16.grocerease;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.content.Intent;
-import android.view.View;
-import android.widget.EditText;
+
+import org.json.JSONException;
+import java.util.*;
+import org.json.*;
+import java.io.*;
+import android.content.res.AssetManager;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import com.jayway.jsonpath.JsonPath;
+
 
 public class BrowseRecipes extends AppCompatActivity {
 
@@ -14,14 +21,66 @@ public class BrowseRecipes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse_recipes);
+
+        ArrayList<String> test = parseRecipes();
+
+        ArrayAdapter adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, test);
+
+        ListView listView = (ListView) findViewById(R.id.grocery_list);
+        listView.setAdapter(adapter);
+
+
     }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, ActivityTwo.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
+
+    public ArrayList<String> parseRecipes(){
+
+        ArrayList<String> recipeInformation = new ArrayList<String>();
+
+        try{
+            JSONObject recipes = new JSONObject(loadJSONFromAsset());
+            String recipes_string = recipes.toString();
+
+            recipeInformation = JsonPath.read(recipes_string, "$.recipes[:10][*].title");
+
+
+        } catch (JSONException e){
+
+            recipeInformation.add("Error loading recipes.");
+
+        }
+
+        return recipeInformation;
+
+
     }
+
+
+    public String loadJSONFromAsset() {
+        String json = null;
+        try {
+            //AssetManager mngr = getAssets();
+            //InputStream is = mngr.open("recipes.json");
+            //InputStream is = getActivity().getAssets().open("recipes.json");
+            AssetManager assetManager = getResources().getAssets();
+            InputStream is = null;
+
+
+            is = assetManager.open("recipes.json");
+
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return json;
+        }
+        return json;
+    }
+
+
+
 }
