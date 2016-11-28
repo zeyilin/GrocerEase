ef944a69aa497b5c12921880f18a0ba28f529393
Chris Chen <23chrischen@gmail.com>
2016-11-28 11:52:59 -0600

    Temp commit with grocery list activity

--- app/src/main/java/com/ee461lteam16/grocerease/SplashActivity.java
+++ app/src/main/java/com/ee461lteam16/grocerease/SplashActivity.java
@@ -1,30 +1,29 @@
 package com.ee461lteam16.grocerease;
 
-import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.content.Intent;
 
 public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-//        Intent intent;
-//        SharedPreferences grocereasePrefs = PreferenceManager.getDefaultSharedPreferences(this);
-//        boolean isLoggedIn = grocereasePrefs.getBoolean("isLoggedIn", false);
-//
-//        if (isLoggedIn) {
-//            intent  = new Intent(this, BrowseRecipes.class);
-//
-//        }
-//        else {
-//            intent = new Intent(this, BrowseRecipes.class);
-//        }
-//
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent;
+        SharedPreferences grocereasePrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean isLoggedIn = grocereasePrefs.getBoolean("isLoggedIn", false);
+
+        if (isLoggedIn) {
+            intent  = new Intent(this, BrowseRecipes.class);
+
+        }
+        else {
+            intent = new Intent(this, BrowseRecipes.class);
+        }
+
+        //Intent intent = new Intent(this, MainActivity.class);
         startActivity(intent);
         finish();
     }
